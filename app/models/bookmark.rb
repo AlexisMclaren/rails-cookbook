8baf3abcdc01ac78df4a6618abe69856841b30a2
Alexis Mclaren mclaren370@gmail.com <mclaren370@gmail.com>
2025-05-12 13:11:26 +0100

    Models validation.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :category
   belongs_to :recipe
+
+  validates :comment, length: { minimum: 6, too_short: 'must have at least 6 characters' }
+  validates :recipe_id, uniqueness: { scope: :category_id, message: 'is already bookmarked in this category' }
 end
